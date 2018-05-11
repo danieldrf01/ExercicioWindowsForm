e5fceba8f3794a25621efb1bad3fbd11e6919622
Marcos Luiz Mittag Filho <Marquinhoszx_xd@Outlook.com>
2018-05-11 16:29:27 -0300

    Exercício 01 utilizando WindowsForm

---
+++ ExercicioWindowsForm/ExercicioWindowsForm/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ExercicioWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnSucessor = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAntecessor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(239, 34);
+            this.txtNumero.Multiline = true;
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(99, 26);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnSucessor
+            // 
+            this.btnSucessor.Location = new System.Drawing.Point(239, 98);
+            this.btnSucessor.Name = "btnSucessor";
+            this.btnSucessor.Size = new System.Drawing.Size(75, 28);
+            this.btnSucessor.TabIndex = 2;
+            this.btnSucessor.Text = "OK";
+            this.btnSucessor.UseVisualStyleBackColor = true;
+            this.btnSucessor.Click += new System.EventHandler(this.btnSucessor_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(22, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sucessor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(22, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Antecessor";
+            // 
+            // btnAntecessor
+            // 
+            this.btnAntecessor.Location = new System.Drawing.Point(239, 154);
+            this.btnAntecessor.Name = "btnAntecessor";
+            this.btnAntecessor.Size = new System.Drawing.Size(75, 23);
+            this.btnAntecessor.TabIndex = 5;
+            this.btnAntecessor.Text = "OK";
+            this.btnAntecessor.UseVisualStyleBackColor = true;
+            this.btnAntecessor.Click += new System.EventHandler(this.btnAntecessor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(726, 436);
+            this.Controls.Add(this.btnAntecessor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSucessor);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnSucessor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAntecessor;
+    }
+}
+
---
+++ ExercicioWindowsForm/ExercicioWindowsForm/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSucessor_Click(object sender, EventArgs e)
+        {
+            int sucessor = Convert.ToInt32(txtNumero.Text);
+            MessageBox.Show("Número sucessor é " + (sucessor + 1));
+        }
+
+        private void btnAntecessor_Click(object sender, EventArgs e)
+        {
+            int antecessor = Convert.ToInt32(txtNumero.Text);
+            MessageBox.Show("Número antecessor é " + (antecessor - 1));
+        }
+
+
+    }
+}
